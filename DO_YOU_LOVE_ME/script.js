95200de0ba4db32b5f3ef5647795c59576f2fee7
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-07-12 14:17:08 +0700

    gui bai tap Do you love me

---
+++ DO_YOU_LOVE_ME/script.js
@@ -0,0 +1,10 @@
+function click_yes(){
+    alert("Wow, i love you to!  ");
+}
+
+function omo_no(){
+    let a = Math.floor(Math.random() * 600);
+    let b = Math.floor(Math.random() * 600);
+    ans_no.style.top = b + 'px';
+    ans_no.style.left= a + 'px'; 
+}
