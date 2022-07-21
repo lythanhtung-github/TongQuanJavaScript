66ca319f85b6093a0e7fa2dd23e354e369c2167c
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-07-21 10:23:03 +0700

    gui bai tap

---
+++ LOOP_TEST/test3.js
@@ -0,0 +1,17 @@
+// for (let i = 1; i < 6; i++) {
+//     for (let j = 1; j < 6; j++) { 
+//         console.log(i+"."+ j);
+//     }
+// }
+
+let i = 1;
+while (i<6)
+{
+    let j = 1;
+    while(j<6)
+    {
+        console.log(`${i}.${j}`);
+        j++;
+    }
+    i++;
+}
