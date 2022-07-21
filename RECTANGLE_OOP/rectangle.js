50be2d44f5bd972c878999ee07912ea0789cf445
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-07-21 15:15:36 +0700

    gui bai tap hcn oop

---
+++ RECTANGLE_OOP/rectangle.js
@@ -0,0 +1,57 @@
+
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
+// ctx.beginPath();
+// ctx.rect(20, 20, 150, 100);
+// ctx.stroke();
+
+class Rectangle {
+    
+    width;
+    height;
+    constructor (width,height){
+        this.width = width;
+        this.height = height;
+    }
+    getWidth() {
+        return this.width;
+    }
+
+    getHeight() {
+        return this.height;
+    }
+
+    setWidth(width) {
+        this.width = width;
+    }
+
+    setHeight(height) {
+        this.height = height;
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+
+    getPerimeter() {
+        return (this.width + this.height) * 2;
+    }
+}
+let rectangle = new Rectangle();
+let width = rectangle.setWidth(400);
+let height = rectangle.setHeight(200);
+width = rectangle.getWidth();
+height = rectangle.getHeight();
+
+let area = rectangle.getArea();
+let perimeter = rectangle.getPerimeter();
+
+let ctx = document.querySelector('#myCanvas').getContext("2d");
+ctx.fillStyle='#fa4b2a';
+ctx.fillRect(0, 0, width, height);
+let c = document.querySelector("#result");
+alert(`Diện tích hình chữ nhật là ${area} và chu vi hình chữ nhật là ${perimeter}`) ;
+
+
+
+
