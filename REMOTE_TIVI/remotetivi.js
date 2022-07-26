1a992dec59d5de8569d5d413da78e3ee5c09b43f
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-07-26 17:34:31 +0700

    gui bai remote-tv

--- REMOTE_TIVI/remotetivi.js
+++ REMOTE_TIVI/remotetivi.js
@@ -2,7 +2,7 @@ class Remote {
     constructor(id, tv) {
         this.id = id;
         this.tv = tv;
-    }
+    };
    
     turnOnTV() {
         if ( this.tv.status == false){
@@ -10,14 +10,16 @@ class Remote {
             document.write("Đã bật Tivi <br>");
         }
         else document.write("Tivi đang bật <br>");
-    }
+    };
+
     turnOffTV(){
         if (this.tv.status == true){ 
             this.tv.status = false;
             document.write("Đã tắt Tivi <br>");
         }
         else document.write("Tivi đang tắt <br>");
-    }
+    };
+
     turnUpVolume() {
         if (this.tv.status == true){
             if (this.tv.volume>0 && this.tv.volume<=100)
@@ -27,9 +29,9 @@ class Remote {
             }
         }
         else document.write(`Tivi đã tắt không thể tăng volume <br>`);
-    }
+    };
 
-    turnDownVolume(){
+    turnDownVolume() {
         if (this.tv.status == true){
             if (this.tv.volume>0 && this.tv.volume<=100)
         {
@@ -38,53 +40,39 @@ class Remote {
         }
         }
         else document.write(`Tivi đã tắt không thể giảm volume <br>`);
-    }
+    }l
 
     changeChannels(channel){
         if (this.tv.status == true){
             this.tv.setChannel(channel);
         }
         else document.write(`Tivi đã tắt không thể chuyển kênh <br>`);
-    }
-}
+    };
+};
 
 class Tivi {
     status;
     constructor(volume, channel){
         this.volume = volume;
         this.channel = channel;
-    }
-
-    // setStatus(status){
-    //     this.status = status;
-    // }
-    // getStatus(){
-    //     return this.status;
-    // }
-
-    // setVolume(volume){
-    //     this.volume = volume;
-    // }
-
-    // getVolume(){
-    //     return this.volume;
-    // }
+    };
 
     setChannel(channel){
         this.channel = channel;
-    }
+    };
+
     getChannel(){
         return this.channel; 
-    }
+    };
 
     tvOn(){
         return this.status = true;
-    }
+    };
 
     tvOff(){
         return this.status = false;
-    }  
-}
+    };
+};
 
 let tv1 = new Tivi(50,7);
 let remote1 = new Remote("remote1", tv1);
@@ -94,7 +82,7 @@ remote1.turnOnTV();
 remote1.turnUpVolume();
 remote1.turnUpVolume();
 remote1.turnUpVolume();
-remote1. turnDownVolume();
+remote1.turnDownVolume();
 remote1.changeChannels("Channel 7");
 document.write(`Đã chuyển channel hiện tại sang kênh ${tv1.getChannel()} <br>`);
 remote1.turnOffTV();
